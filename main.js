201772a7f4ee36132e90c18a7c5f1100d5be6ee3
Walison <walyteodoro@gmail.com>
2020-07-30 23:22:02 -0300

    up

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 alert('Test')
+/******************************* ex 1 *******************************/
+// class teste {
+//     metodo() {
 
-class teste {
-    metodo() {
-
-    }
-    outroMetodo() {
+//     }
+//     outroMetodo() {
          
-    }
-}
+//     }
+// }
+
+/******************************* ex 2 *******************************/
+const usuarios = [
+    {nome: 'Diego', idade: 23, empresa: 'Rocketseat'},
+    {nome: 'Gabriel', idade: 15, empresa: 'Rocketseat'},
+    {nome: 'Lucas', idade: 30, empresa: 'Facebook'}
+]
+
+/********MAP********/
+
+let ages = usuarios.map
+console.log(usuarios)
+
+
+
