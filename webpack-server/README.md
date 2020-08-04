539a67dec6e0d67012b9c5d7b40145f1331e1fdd
Walison <walyteodoro@gmail.com>
2020-08-04 12:11:01 -0300

    up

---
+++ webpack-server/README.md
@@ -0,0 +1,2 @@
+# Rocketseat cursoJS  ES6 STARTER Webpack Server
+ Eis minhas tentativas aos desafios desta etapa
---
+++ webpack-server/src/main.js
@@ -0,0 +1,10 @@
+// EXERCÍCIO 1.1
+import ClasseUsuario from './funcoes'
+ClasseUsuario.info()
+
+// EXERCÍCIO 1.2
+import { idade } from './funcoes'
+console.log('EX1.2   ' + idade)
+
+//  EXERCÍCIO 1.3
+import Usuario, { idade as IdadeUsuario } from './funcoes'
